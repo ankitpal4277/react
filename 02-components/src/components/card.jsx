7efb3d127abab40a9b58c1211cf37bf9db54bcc2
Ankit Pal <ankitpalsingh0208@gmail.com>
2026-09-08 13:49:07 +0530

    Components in React

---
+++ 02-components/src/components/card.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const card = () => {
+  return (
+    <div>
+      <div className='card'>
+        <h1>Ankit Pal</h1>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eveniet, incidunt?</p>
+      </div>
+    </div>
+  )
+}
+
+export default card

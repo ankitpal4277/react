1c1bada3c23c8927accf5dbfe61043664b659e3d
Ankit Pal <ankitpalsingh0208@gmail.com>
2026-09-09 23:34:03 +0530

    Fundamentals of using attributes in React

---
+++ 03-props/src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Card from './components/Card'
+
+const App = () => {
+  return (
+    <div className="parent">
+      
+
+      <Card user="Ankit" />
+
+      <Card />
+      
+    </div>
+  )
+}
+
+export default App
